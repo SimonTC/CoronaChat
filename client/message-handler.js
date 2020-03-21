0e8ce363a70b4d71f9feaf8a1a1d283a2ec30e73
Simon Clement <simon@simontc.com>
2020-03-21 14:06:52 +0200

    Change socket

--- client/message-handler.js
+++ client/message-handler.js
@@ -22,7 +22,7 @@ export default class MessageHandler {
   }
 
   get SOCKET_SERVER() {
-    return window.location.protocol + "//" + window.location.hostname + ":" + SOCKET_SERVER.PORT;
+    return "ws//" + window.location.hostname + ":" + SOCKET_SERVER.PORT;
   }
 
   _initializeSocket() {
