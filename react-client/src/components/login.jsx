2c7cdb5e7bf96c10461a69b8902e1e8675c37b31
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2020-03-21 17:50:24 +0200

    Call Addparticipant from login

--- react-client/src/components/login.jsx
+++ react-client/src/components/login.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
 import "../App.css";
+import {addParticipant} from "../state/participants";
 
 class Login extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {playerName: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    //alert('Your favorite flavor is: ' + this.state.playerName);
+
+    const user = {
+      name: this.state.playerName,
+      position: {
+        posx: 100,
+        posy: 100,
+      },
+    };
+
+    addParticipant(user);
+    event.preventDefault();
+  }
+
+  handleChange(event){
+    this.setState({playerName: event.target.value});
+  }
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <div className="header">
           <h1>CORONA PARTY</h1>
         </div>
 
-        <input type="text" placeholder="NAME" name="playername" required />
+        <input type="text" placeholder="NAME" name="playername" onChange={this.handleChange} required/>
         <button type="submit">ENTER</button>
-      </div>
+      </form>
     );
   }
 }
