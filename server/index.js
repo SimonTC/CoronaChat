c3d3fedf9d79bf514120c2ed93dfc316f8dd12e7
saurio <jussi.saurio.ext@nokia.com>
2020-03-21 12:17:01 +0200

    Initial socket.io refactor
    Did not really test this at all, kek

--- server/index.js
+++ server/index.js
@@ -1,55 +1,58 @@
-const ws = require("ws");
-const config = require("../config/index.json");
-
-const socketServer = new ws.Server({
-  port: config.socketServerPort,
-  perMessageDeflate: false
+// Server setup
+const server = require("http").createServer(req => {});
+server.listen(3000, () => {
+  console.log("Listening on 3000");
 });
 
-const connectedSockets = [];
+const io = require("socket.io")(server, {
+  path: "/chat",
+  serveClient: false,
+  cookie: "coronachat"
+});
 
-socketServer.on("connection", _onConnectionEstablished);
-socketServer.on("error", _onConnectionError);
+const users = {};
 
-function _onConnectionEstablished(socket) {
-  console.info("Client has connected.");
+const EVENTS = {
+  CONNECTED: "CONNECTED",
+  DISCONNECTED: "DISCONNECTED",
+  MOVE: "MOVE",
+  MESSAGE: "MESSAGE"
+};
 
-  if (connectedSockets.length >= config.maxConnections) {
-    console.info("Server is full");
+io.on("connection", socket => {
+  // Store user socket and world position in memory
+  users[socket.id] = {
+    socket,
+    position: {}
+  };
 
-    socket.close();
-    return;
-  }
+  // Inform everyone about connection of user
+  socket.emit(EVENTS.CONNECTED, socket.id);
+  socket.broadcast.emit(EVENTS.CONNECTED, socket.id);
 
-  socket.on("move", _onMove);
-
-  socket.on("message", message => _onMessage(message, socket));
-
-  socket.on("error", _closeConnection);
-  socket.on("close", _closeConnection);
-
-  connectedSockets.push(socket);
-}
-
-function _onMessage(message, sender) {
-  connectedSockets.forEach(function each(client) {
-    if (client !== sender && client.readyState === ws.OPEN) {
-      client.send(message);
-    }
+  // Handle event when user is about to disconnect (for possible cleanups)
+  socket.on("disconnecting", s => {
+    // Do something maybe
   });
-}
 
-function _onMove(message) {
-  console.info("Somebody is moving");
-}
+  // Handle disconnection of client
+  socket.on("disconnect", s => {
+    delete users[socket.id];
+    socket.emit(EVENTS.DISCONNECTED, socket.id);
+    socket.broadcast.emit(EVENTS.DISCONNECTED, socket.id);
+  });
 
-function _onConnectionError(error) {
-  console.error(`Unhandled error code: ${error}.`);
+  // Log errors
+  socket.on("error", console.error);
 
-  process.exit(1);
-}
+  // Store move and broadcast to other clients
+  socket.on(EVENTS.MOVE, pos => {
+    users[socket.id].position = pos;
+    socket.broadcast.emit(EVENTS.MOVE, socket.id, pos);
+  });
 
-function _closeConnection(socket) {
-  console.info("Connection closing");
-  // TODO: Handle player leave
-}
+  // Broadcast msg to other clients
+  socket.on(EVENTS.MESSAGE, msg => {
+    socket.broadcast.emit(EVENTS.MESSAGE, msg);
+  });
+});
